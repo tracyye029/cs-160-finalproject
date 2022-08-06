99ca29125533f6ce8debd0bf4d315d19ff33dddc
Isabel Li <ilyues@berkeley.edu>
2022-08-06 11:27:26 -0700

    feat: routing, start screen which redirects to home after 5 secs

--- App.js
+++ App.js
@@ -14,31 +14,42 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-
 //specify that we want to run our website on 'http://localhost:8000/'
 const host = 'localhost';
 const port = 8000;
 
 var publicPath = path.join(__dirname, 'public'); //get the path to use our "public" folder where we stored our html, css, images, etc
-app.use(express.static(publicPath));  //tell express to use that folder
-
-
+app.use(express.static(publicPath)); //tell express to use that folder
 
 //here's where we specify what to send to users that connect to our web server...
 //if there's no url extension, it will show "index.html"
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/"));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/'));
 });
 
-//depending on what url extension the user navigates to, send them the respective html file. 
-/* // this example app has no additional pages besides the index, so I've commented these out for now.
-app.get('/a', function (req, res) {
-    res.sendFile(publicPath + '/a.html');
+//depending on what url extension the user navigates to, send them the respective html file.
+// this example app has no additional pages besides the index, so I've commented these out for now.
+
+app.get('', function (req, res) {
+  res.sendFile(publicPath + '/start.html');
 });
-*/
+app.get('/home', function (req, res) {
+  res.sendFile(publicPath + '/home.html');
+});
+app.get('/scan', function (req, res) {
+  res.sendFile(publicPath + '/scan.html');
+});
+app.get('/tips', function (req, res) {
+  res.sendFile(publicPath + '/tips.html');
+});
+app.get('/wishlist', function (req, res) {
+  res.sendFile(publicPath + '/wishlist.html');
+});
+app.get('/index', function (req, res) {
+  res.sendFile(publicPath + '/index.html');
+});
 
-
-//run this server by entering "node App.js" using your command line. 
+//run this server by entering "node App.js" using your command line.
 app.listen(port, () => {
-     console.log(`Server is running on http://${host}:${port}`);
-   });
+  console.log(`Server is running on http://${host}:${port}`);
+});
